4f0f07f93a191b2c9d65860ed16ca8c23d863792
itai rozen <itai.rozen@mail.huji.ac.il>
2021-11-07 12:58:36 +0200

    added js Ex.

--- javascript/js-071121/1.1-primitive_data_types.js
+++ javascript/js-071121/1.1-primitive_data_types.js
@@ -1,13 +1,13 @@
-let str = "string"
-let num = 1
+let string = "string"
+let number = 1
 let obj = {value: 1}
 let isBoolean = true
 let nullVariable = null
 let undefinedVariable
 let symVariable = Symbol('this is a symbol')
 
-console.log(typeof str)
-console.log(typeof num)
+console.log(typeof string)
+console.log(typeof number)
 console.log(typeof obj)
 console.log(typeof isBoolean)
 console.log(typeof nullVariable)
