fcad39e811ffac015a9d3eb8a23606417539a46d
itai rozen <itai.rozen@mail.huji.ac.il>
2021-11-07 12:00:34 +0200

    added js Ex

---
+++ javascript/js-071121/1.1-primitive_data_typses.js
@@ -0,0 +1,15 @@
+let str = "string"
+let num = 1
+let obj = {value: 1}
+let isBoolean = true
+let nullVariable = null
+let undefinedVariable
+let symVariable = Symbol('this is a symbol')
+
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof obj)
+console.log(typeof isBoolean)
+console.log(typeof nullVariable)
+console.log(typeof undefinedVariable)
+console.log(typeof symVariable)
