46abc57037574502a17a084ad6a381190b4e6d93
itai rozen <itai.rozen@mail.huji.ac.il>
2021-11-24 18:50:10 +0200

    fixed 19.1 ex

--- frontend/fronend-241121/19.1/script.js
+++ frontend/fronend-241121/19.1/script.js
@@ -5,15 +5,15 @@
 // 5. Change the text of the <p> element ot something else of your
 
 
-let liElement = document.querySelector('ul')
-liElement.querySelector('.start-here').textContent = 'main title'
-let anotherLiElement = document.createElement('li')
-anotherLiElement.textContent = "sub title 4"
-let childUl = liElement.querySelector('ul')
-console.log(childUl)
-childUl.appendChild(anotherLiElement)
-let listElements = document.querySelectorAll('li')
 let ulElement = document.querySelector('ul')
+ulElement.querySelector('.start-here').textContent = 'main title'
+let liElement = document.createElement('li')
+liElement.textContent = "sub title 4"
+let childUl = ulElement.querySelector('ul')
+console.log(childUl)
+childUl.appendChild(liElement)
+let listElements = document.querySelectorAll('li')
+console.log(listElements)
 ulElement.removeChild(listElements[listElements.length-1])
 document.title = "Master of the Dom"
 document.querySelector('p').textContent = 'Shalom Olam'
